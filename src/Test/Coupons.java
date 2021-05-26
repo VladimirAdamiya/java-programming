2d4d0631618f6b430add2c2313616865463806f8
VladimirAdamiya <adamiya.v@gmail.com>
2021-05-25 20:28:02 -0500

    Coupons

--- src/Test/Coupons.java
+++ src/Test/Coupons.java
@@ -8,12 +8,20 @@ public class Coupons {
         //Write your code here
 
         Scanner scan = new Scanner(System.in);
-        String coupon = scan.nextLine();
-
         System.out.println("Enter number of coupons: ");
+        int couponCNT = scan.nextInt();
 
+        if (couponCNT < 3) {
+            System.out.println("Not enough coupons");
+        } else {
 
+            int candyCount = couponCNT/10;
+            int remainingCoupons = couponCNT % 10;
+            int gumbollsCount = remainingCoupons / 3;
 
+            System.out.println("Number of Candies: " + candyCount);
+            System.out.println("Number of Gumballs:" + gumbollsCount);
+        }
     }
 }
 
---
+++ src/Test/LeasingOffice.java
@@ -0,0 +1,20 @@
+package Test;
+
+import java.util.Scanner;
+
+public class LeasingOffice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Cybertek Apartments!");
+        System.out.println("Number of bedrooms you are interested:");
+
+        int numberOfBedrooms = scan.nextInt();
+        int startingPrice = 0;
+
+        //WRITE YOUR CODE HERE:
+    }
+
+
+
+}
+
