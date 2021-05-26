d2f2c5942f503dd22d0d8e3cd0d9b6023418c580
VladimirAdamiya <adamiya.v@gmail.com>
2021-05-25 20:06:09 -0500

    Coupons

---
+++ src/Test/Coupons.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Coupons {
+    public static void main(String[] args) {
+        //Write your code here
+
+        Scanner scan = new Scanner(System.in);
+        String coupon = scan.nextLine();
+
+        System.out.println("Enter number of coupons: ");
+
+
+
+    }
+}
+
