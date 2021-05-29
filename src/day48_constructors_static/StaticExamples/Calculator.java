0ad4e32c05f91375798b1f3aaf04cd1b8fe82ef9
VladimirAdamiya <adamiya.v@gmail.com>
2021-05-29 18:21:30 -0500

    Initial commit

--- src/day48_constructors_static/StaticExamples/Calculator.java
+++ src/day48_constructors_static/StaticExamples/Calculator.java
@@ -1,12 +1,13 @@
 package day48_constructors_static.StaticExamples;
 
 public class Calculator {
-    // static method
+    //static method
     public static void add(int num1, int num2) {
         System.out.println(num1 + num2);
     }
-    // instance/
+    //instance/object method
     public void multiply(int num1, int num2) {
         System.out.println(num1 * num2);
     }
+
 }
