79ec16f7c0fa2e0069972777fe5de14c3829ada8
VladimirAdamiya <adamiya.v@gmail.com>
2021-06-13 09:46:41 -0500

    Initial commit

---
+++ src/day52_inheritance/Discord.java
@@ -0,0 +1,13 @@
+package day52_inheritance;
+
+public class Discord extends MobileApp {
+    public void chat(String someone) {
+        System.out.println("Chatting with " + someone +" on Discord");
+    }
+
+    @Override
+    public void useTheApp(int minutes) {
+        super.useTheApp(minutes);
+        chat("Vladislav");
+    }
+}
